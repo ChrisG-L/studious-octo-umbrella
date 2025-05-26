1e1b9798b34023d1f1ef411c0efbd159212e5cd6
Chris G--L | WSL <christophe.gerardin@epitech.eu>
2025-05-26 08:07:21 +0200

    Base | CI:Ok

---
+++ Makefile
@@ -0,0 +1,36 @@
+SRC =	main.cpp \
+
+NAME = factorial
+
+OBJDIR = build
+OBJ = $(SRC:%.cpp=$(OBJDIR)/%.o)
+
+INC =	
+LIB =	
+
+CXX = g++
+CXXFLAGS = -Wall -Wextra -g3
+
+CXXFLAGS += $(INC:%=-I %)
+CXXFLAGS += $(LIB:%=-%)
+
+all: $(NAME)
+
+$(NAME): $(OBJ)
+	$(CXX) -o $(NAME) $(OBJ) $(CXXFLAGS)
+
+$(OBJDIR)/%.o: %.cpp
+	@mkdir -p $(dir $@)
+	$(CXX) -o $@ -c $< $(CXXFLAGS)
+
+run: $(NAME)
+	@echo "--------------------------------"
+	@./$(NAME)
+
+clean:
+	rm -rf $(OBJDIR)
+
+fclean: clean
+	rm -rf $(NAME)
+
+re: fclean all
---
+++ main.cpp
@@ -0,0 +1,38 @@
+/*
+** EPITECH PROJECT, 2025
+** Workflow
+** File description:
+** main
+*/
+
+#include <iostream>
+#include <string>
+
+int factorial(int n) {
+    if (n == 0) {
+        return 1;
+    }
+    return n * factorial(n - 1);
+}
+
+int main(int ac, char **av)
+{
+    if (ac != 2 || std::string(av[1]) == "--help") {
+        std::cerr << "Usage: " << av[0] << " <number>" << std::endl;
+        return (ac != 2) ? 84 : 0;
+    }
+    int number = 0;
+    try {
+        number = std::stoi(av[1]);
+    } catch (const std::exception &e) {
+        std::cerr << "Error: Invalid number." << std::endl;
+        return 84;
+    }
+    if (number < 0) {
+        std::cerr << "Error: Number must be non-negative." << std::endl;
+        return 84;
+    }
+    int result = factorial(number);
+    std::cout << "Factorial of " << number << " is " << result << std::endl;
+    return 0;
+}
